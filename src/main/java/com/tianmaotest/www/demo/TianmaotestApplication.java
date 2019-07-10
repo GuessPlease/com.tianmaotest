3a7828e8ff94b1a9321ba3555d50800b07e78c34
GuessPlease <52537863+GuessPlease@users.noreply.github.com>
2019-07-10 20:21:08 +0800

    init repo

---
+++ src/main/java/com/tianmaotest/www/demo/TianmaotestApplication.java
@@ -0,0 +1,13 @@
+package com.tianmaotest.www.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TianmaotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TianmaotestApplication.class, args);
+    }
+
+}
